a46a1c0cb89e301f423082cee3225ec8a0f84945
Mohammad Momeni <59508036+mdmomeni@users.noreply.github.com>
2021-12-26 16:45:17 +0330

    Completing the process

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/rsa"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -13,83 +14,171 @@ import (
 )
 
 const (
-	alg         = "RS256"
 	privKeyPath = "private.pem"
 	pubKeyPath  = "public.pem"
+	cookieKey   = "token"
 )
 
 var (
-	verifyKey *rsa.PublicKey
-	signKey   *rsa.PrivateKey
+	privateKey *rsa.PrivateKey
+	publicKey  *rsa.PublicKey
 )
 
-func fatal(err error) {
+func tokenExpiration() time.Time{
+	// return time.Now().AddDate(0, 0, 1)
+	return time.Now().Add(time.Second * 40)
+}
+
+func setPrivateKey() {
+	privKeyBytes, err := ioutil.ReadFile(privKeyPath)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Print(err.Error())
+		log.Fatal("Error while reading the PRIVATE KEY using `ReadFile`")
 	}
+	priv, err := jwt.ParseRSAPrivateKeyFromPEMWithPassword(privKeyBytes, "moehmeni")
+	if err != nil {
+		fmt.Print(err.Error())
+		log.Fatal("\nError while parsing the PRIVATE KEY using `jwt.ParseRSAPrivateKeyFromPEM`")
+	}
+
+	privateKey = priv
 }
 
-func init() {
-	signBytes, err := ioutil.ReadFile(privKeyPath)
-	fatal(err)
+func setPublicKey() {
+	publicKeyBytes, err := ioutil.ReadFile(pubKeyPath)
+	if err != nil {
+		fmt.Print(err.Error())
+		log.Fatal("Error while reading the PUBLIC KEY using `ReadFile`")
+	}
 
-	signKey, err = jwt.ParseRSAPrivateKeyFromPEM(signBytes)
-	fatal(err)
+	pub, err := jwt.ParseRSAPublicKeyFromPEM(publicKeyBytes)
 
-	verifyBytes, err := ioutil.ReadFile(pubKeyPath)
-	fatal(err)
+	if err != nil {
+		fmt.Print(err.Error())
+		log.Fatal("\nError while parsing the PUBLIC KEY using `jwt.ParseRSAPrivateKeyFromPEM`")
+	}
 
-	verifyKey, err = jwt.ParseRSAPublicKeyFromPEM(verifyBytes)
-	fatal(err)
-}
-
-// Define some custom types were going to use within our tokens
-type UserInfo struct {
-	Username string
+	publicKey = pub
 }
 
 func createToken(username string) (string, error) {
 
-	token := jwt.New(jwt.GetSigningMethod(alg))
-	token.Claims = jwt.StandardClaims{
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.StandardClaims{
 		Subject:   username,
-		ExpiresAt: time.Now().AddDate(0, 0, 1).Unix(),
-	}
+		ExpiresAt: tokenExpiration().Unix(),
+	})
 
-	return token.SignedString(signKey)
+	return token.SignedString(privateKey)
+}
+
+// Since we only use the one private key to sign the tokens,
+// we also only use its public counter part to verify
+// [Resource](https://github.com/dgrijalva/jwt-go/blob/master/http_example_test.go#L107)
+func keyFunc(token *jwt.Token) (interface{}, error) {
+	return publicKey, nil
 }
 
 // This endpoint recieve a cookie from user (client)
 // and verifies that the cookie is signed and unexpired
 // it'll return the username itself as plain text if the cookie is ok
-func verify(w http.ResponseWriter, r *http.Request) {
-	// jwtCookie := r.Header.Get("Cookie")
-	// w.Write([]byte())
+func verify(res http.ResponseWriter, r *http.Request) {
+
+	// Get `token` cookie in the headers
+	var jwtStringInCookie string
+	requestCookies := r.Cookies()
+	for _, cookie := range requestCookies {
+		if cookie.Name == cookieKey {
+			jwtStringInCookie = cookie.Value
+		}
+	}
+
+	// Get username to verify somehow!
+	username := r.Header.Get("x-username")
+
+	if jwtStringInCookie == "" {
+		res.WriteHeader(http.StatusBadRequest)
+		res.Write([]byte("JWT token cookie is not provided in the request header"))
+		return
+	}
+	if username == "" {
+		res.WriteHeader(http.StatusBadRequest)
+		res.Write([]byte("Username is not provided in the request header"))
+		return
+	}
+
+	claims := jwt.StandardClaims{
+		Subject: username,
+	}
+	token, err := jwt.ParseWithClaims(jwtStringInCookie, &claims, keyFunc)
+
+	if err != nil {
+		v, _ := err.(*jwt.ValidationError)
+
+		// Respond to token expiraiton
+		if v.Errors == jwt.ValidationErrorExpired {
+			res.WriteHeader(http.StatusUnauthorized)
+			res.Write([]byte(claims.Valid().Error()))
+			return
+		}
+
+		res.WriteHeader(http.StatusBadRequest)
+		res.Write([]byte("Failed to parse token " + claims.Valid().Error()))
+		return
+	}
+
+	// Check the token validation at first
+	if !token.Valid {
+		res.WriteHeader(http.StatusUnauthorized)
+		res.Write([]byte("Token is not valid"))
+		return
+	}
+
+	// Then check claims validation
+	tokenClaim := token.Claims.(*jwt.StandardClaims)
+
+	// Respond to different subjects(usernames)
+	// (of course we can add more configs for some needs such as admin access)
+	tokenSubjectOk := tokenClaim.Subject == username
+	if !tokenSubjectOk {
+		res.WriteHeader(http.StatusForbidden)
+		res.Write([]byte("You're not allowed to use this token"))
+		return
+	}
+
+	res.Write([]byte(username + " " + time.Unix(tokenClaim.ExpiresAt, 0).String()))
 }
 
 // This endpoint creates a JWT and sends it back to the user as a cookie.
 // The body of the response will be the RSA public key in plain text.
-func authHandler(w http.ResponseWriter, r *http.Request) {
+func authHandler(res http.ResponseWriter, r *http.Request) {
 	username := chi.URLParam(r, "username")
 	jwtCookie, err := createToken(username)
 
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Printf("Token Signing error: %v\n", err)
+		res.WriteHeader(http.StatusInternalServerError)
+		res.Write([]byte("Token Signing error: " + err.Error()))
 		return
 	}
 
-	tomorrowDate := time.Now().AddDate(0, 0, 1)
-	cookieValue := "token=" + jwtCookie + "; Expires=" + tomorrowDate.String()
-	r.Header.Set("Set-Cookie", cookieValue)
-	w.Write(verifyKey.N.Bytes())
+	cookie := http.Cookie{Name: cookieKey, Value: jwtCookie}
+	http.SetCookie(res, &cookie)
+
+	pub := "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2j3jHc7292PvBK9YafZfIB5cD2Ke+VUSfGWsP/zff0gkN6iqLt1VYziNxzSQnE7X1viunKUAEosfUokn9tRSSVcpwt+O+y/in3nNapjX0UA5f/dwlsJss3mRkrJF35JkFwJ6yDKRiFxdf4QXUjWYNlusUE9Im4heHx19TIXUoRqMZHrnudTntO0Ne3NhWfdcdOndes110PR28yUiH88r0Qmntix7GUC+oN3WiHPRIlv17znch7UqNedWThpxBWDufsgJa5CxzTRT27WQ5r+3vASDbcg4UMpIxYeAuzkIx2xEdp43fw5/BGbctjs9lMmpavCHgevZFAXhSmCcii0u9QIDAQAB"
+	res.Write([]byte(pub))
+}
+
+func setPrivateAndPublicKeys() {
+	setPrivateKey()
+	setPublicKey()
 }
 
 func main() {
+
+	// Initilize the RSA keys once
+	setPrivateAndPublicKeys()
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-
-	// Get requests
 	r.Get("/verify", verify)
 	r.Get("/auth/{username}", authHandler)
 
